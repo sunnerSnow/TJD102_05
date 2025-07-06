e811d4cbc9afebef4614e98547847fcc891dcd4e
summerSnow <lovepeace.yuki@gmail.com>
2025-07-06 15:31:43 +0800

    增加nav的js

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,13 @@
 
 window.addEventListener('scroll', function() {
   const nav = document.querySelector('.nav');
+  const logoImg = document.querySelector('#navbar-logo')
   if (window.scrollY > 60) { // 捲動超過 60px，加上 class
     nav.classList.add('scrolled');
+    logoImg.src='./images/logo-white.svg';
   } else {
     nav.classList.remove('scrolled');
+    logoImg.src = './images/logo-black.svg';
   }
 });
+

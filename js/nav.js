e679578b01e8f1f395dfc38ac251b64c0fe28bec
summerSnow <lovepeace.yuki@gmail.com>
2025-07-11 17:28:58 +0800

    add hamburger menu

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,8 @@
 window.addEventListener('scroll', function() {
   const nav = document.querySelector('.nav');
   const logoImg = document.querySelector('#navbar-logo')
+  //如果螢幕寬小於768就不執行(手機版)
+  if (window.innerWidth <= 768) return;
   if (window.scrollY > 60) { // 捲動超過 60px，加上 class
     nav.classList.add('scrolled');
     logoImg.src='./images/logo-white.svg';
@@ -11,3 +13,18 @@ window.addEventListener('scroll', function() {
   }
 });
 
+
+const hamburger = document.getElementById('hamburger')
+const menuOverlay = document.getElementById('menuOverlay')
+
+hamburger.addEventListener('click' , () => {
+  hamburger.classList.toggle('active');
+  menuOverlay.classList.toggle('open')
+})
+
+menuOverlay.addEventListener('click', (e) => {
+  if(e.target === menuOverlay){
+    hamburger.classList.remove('active');
+   menuOverlay.classList.remove('open')
+  }
+})
